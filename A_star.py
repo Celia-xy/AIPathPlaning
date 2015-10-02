149b07b1cf2b03f561c8f0dbc9ef23690884d8ca
Celia-xy <yaooxii@gmail.com>
2015-10-01 23:01:33 -0400

    Astar

--- A_star.py
+++ A_star.py
@@ -338,3 +338,17 @@ def draw_path(maze_grid, path):
                 canvas.create_rectangle((c+1)*5+1, (r+1)*5+1, (c+2)*5, (r+2)*5, fill="green")
 
     screen.mainloop()
+
+# test
+my_maze = create_maze_dfs(101, 101)
+(start, goal, my_maze) = set_state(my_maze)
+draw_grid(my_maze)
+# get path by A* search
+(PATH, reach_goal, expanded) = A_star_forward(start, goal, my_maze, "large", "forward")
+
+# if path exist, draw maze and path
+if reach_goal:
+    draw_path(my_maze, PATH)
+    print expanded
+else:
+    print "Path doesn't exist"
